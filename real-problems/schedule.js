b1739b7ebed46863aa09f53a52ae564c56b68b76
郑俊鑫 <18810544291@163.com>
2017-03-06 18:57:56 +0800

    完成函数式的重构，但好像比非函数式复杂多了

--- real-problems/schedule.js
+++ real-problems/schedule.js
@@ -31,8 +31,8 @@ const data = [
 // })
 
 const genHasDiscount = R.any(R.compose(R.equals(2), R.prop('type')))
-// const processSchedules = R.map(item => R.assoc('hasDiscount', genHasDiscount(item.prices), item))
-const processSchedules = R.map(R.converge(R.assoc('hasDiscount'), [item => genHasDiscount(item.prices), R.identity]))
+const addHasDiscount = R.converge(R.assoc('hasDiscount'), [R.compose(genHasDiscount, R.prop('prices')), R.identity])
+const processSchedules = R.map(addHasDiscount)
 
 const schedules = processSchedules(data)
 console.log(schedules.map(schedule => schedule.hasDiscount)) // [false, true]
