649b967e2ab25cf52a344eee174111cb83c349d5
郑俊鑫 <18810544291@163.com>
2017-03-06 12:58:36 +0800

    add more annotation

--- egghead/converge.js
+++ egghead/converge.js
@@ -1,7 +1,8 @@
 // 更新对象
 // 优化方式
 // 1、使用 propOr 设置默认值
-// 2、使用 converge 减少 person 参数的出现
+// 2、使用 compose 
+// 3、使用 converge 减少 person 参数的出现
 // 由：const getUpdatedPerson = (person) => R.assoc('avatar', getWithDefault(person), person)
 // 到：const getUpdatedPerson = R.converge(R.assoc('avatar'), [getWithDefault, R.identity])
 const R = require('ramda')
