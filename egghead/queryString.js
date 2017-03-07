8a82bfc16955548f1b8c57ebb2c78cfbce3ed828
郑俊鑫 <18810544291@163.com>
2017-03-07 12:46:41 +0800

    use fp way

--- egghead/queryString.js
+++ egghead/queryString.js
@@ -25,7 +25,10 @@ const queryString = '?page=2&pageSize=10&total=203'
 
 // egghead写的
 const parseQs = R.compose(
-    R.tail,
+    R.fromPair,
+    R.map(R.split('=')),
+    R.split('&'),
+    R.tail
 )
 
 const result = parseQs(queryString)
