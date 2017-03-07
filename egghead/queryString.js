222a478b7e3c7cd4316eb9a5ba14728ed8adae4e
郑俊鑫 <18810544291@163.com>
2017-03-07 12:37:42 +0800

    use ordinary function

--- egghead/queryString.js
+++ egghead/queryString.js
@@ -1,7 +1,19 @@
+const R = require('ramda')
+
 const queryString = '?page=2&pageSize=10&total=203'
 
+// const parseQs = qs => {
+//     const tail = qs.slice(1)
+//     const pairs = tail.split('&')
+//     return pairs.reduce((acc, pair) => {
+//         const [key, value] = pair.split('=')
+//         acc[key] = value
+//         return acc
+//     }, {})
+// }
+
 const parseQs = qs => {
-    const tail = qs.slice(1)
+    const tail = R.tail(qs)
     const pairs = tail.split('&')
     return pairs.reduce((acc, pair) => {
         const [key, value] = pair.split('=')
