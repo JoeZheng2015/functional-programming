13d39df46fd2eba88482e81fd0e6403d37e41713
郑俊鑫 <18810544291@163.com>
2017-03-07 12:36:47 +0800

    use ordinary function

---
+++ egghead/queryString.js
@@ -0,0 +1,14 @@
+const queryString = '?page=2&pageSize=10&total=203'
+
+const parseQs = qs => {
+    const tail = qs.slice(1)
+    const pairs = tail.split('&')
+    return pairs.reduce((acc, pair) => {
+        const [key, value] = pair.split('=')
+        acc[key] = value
+        return acc
+    }, {})
+}
+
+const result = parseQs(queryString)
+console.log(result)
