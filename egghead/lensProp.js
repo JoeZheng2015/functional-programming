6efe06bf5fd5b651c47ff1b819f54869ac891b9b
郑俊鑫 <18810544291@163.com>
2017-03-15 20:11:23 +0800

    非函数式实现

---
+++ egghead/lensProp.js
@@ -0,0 +1,15 @@
+// 查值、赋值、函数赋值
+const R = require('ramda')
+
+const person = {
+    firstName: 'Fred',
+    lastName: 'Flintstone',
+}
+
+const view = (prop, target) => target[prop]
+const set = (prop, value, target) => Object.assign({}, target, {[prop]: value})
+const over = (prop, action, target) => Object.assign({}, target, {[prop]: action(prop)})
+
+const result = over('firstName', R.toUpper, person)
+
+console.log(result)
