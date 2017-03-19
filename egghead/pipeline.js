d91b6529915f7394905a747e59a57913f4a3fbd8
郑俊鑫 <18810544291@163.com>
2017-03-19 12:22:55 +0800

    函数式编程

--- egghead/pipeline.js
+++ egghead/pipeline.js
@@ -1,3 +1,7 @@
+// 取数组排序后的第一个元素的名字
+
+const R = require('ramda')
+
 const teams = [
     {name: 'Lions', score: 5},
     {name: 'Tigers', score: 4},
@@ -6,7 +10,9 @@ const teams = [
 ]
 
 // 非函数式
-const getTopName = (teams) => teams.sort((a, b) => b.score - a.score)[0].name
+// const getTopName = (teams) => teams.sort((a, b) => b.score - a.score)[0].name
+
+const getTopName = R.pipe(R.sort(R.descend(R.prop('score'))), R.head, R.prop('name'))
 
 const result = getTopName(teams)
 console.log(result)
