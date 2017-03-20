97a9554dba40b46ef701c9dd6accb835854ac76a
郑俊鑫 <18810544291@163.com>
2017-03-20 19:12:45 +0800

    add pickAll

--- egghead/pick.js
+++ egghead/pick.js
@@ -4,12 +4,19 @@ const product = {
     stock: 1,
 }
 
-const getProps = props => target => props.reduce((acc, prop) => {
+const pick = props => target => props.reduce((acc, prop) => {
     if (target.hasOwnProperty(prop)) {
         acc[prop] = target[prop]
     }
     return acc
 }, {})
 
-const result = getProps(['name', 'price', 'haha'])(product)
+const pickAll = props => target => props.reduce((acc, prop) => {
+    acc[prop] = target[prop]
+    return acc
+}, {})
+
+const getProps = pickAll(['name', 'price', 'haha'])
+
+const result = getProps(product)
 console.log(result)
