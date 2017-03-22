280225156ac7d4b6ae427c40a8c9d7a1d02472cd
郑俊鑫 <18810544291@163.com>
2017-03-22 12:54:28 +0800

    add curry function

--- egghead/curryAndUncurry.js
+++ egghead/curryAndUncurry.js
@@ -1,6 +1,18 @@
 // 自己实现 curry 和 uncurry 函数
 
 const add = (a, b) => a + b
+const curry = fn => {
+    let fnLength = fn.length
+    let args = []
 
-const result = add(1, 2)
+    return function curryFunction(...options) {
+        args = args.concat(options)
+        fnLength = fnLength - options.length
+
+        return fnLength > 0 ? curryFunction : fn.apply(null, args)
+    }
+}
+const curryAdd = curry(add)
+
+const result = curryAdd(1)(2)
 console.log(result)
