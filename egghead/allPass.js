b7ba9c28b6ac12c9ff9ea4be781546e7b9ce5e1a
郑俊鑫 <18810544291@163.com>
2017-04-30 12:47:43 +0800

    封装成predicates

--- egghead/allPass.js
+++ egghead/allPass.js
@@ -1,3 +1,6 @@
+// 多条件筛选
+
+const R = require('ramda')
 const cars = [
     {
         name: 'suv',
@@ -22,15 +25,21 @@ const cars = [
 ]
 
 // 非函数式编程
-const result = cars.filter(car => {
-    const goodMilage = car => car.mpg >= 30
-    const fourDoors = car => car.doors === 4
-    const predicates = [
-        goodMilage,
-        fourDoors,
-    ]
+// const result = cars.filter(car => {
+//     const goodMilage = car => car.mpg >= 30
+//     const fourDoors = car => car.doors === 4
+//     const predicates = [
+//         goodMilage,
+//         fourDoors,
+//     ]
 
-    return predicates.every(predicate => predicate(car))
-})
+//     return predicates.every(predicate => predicate(car))
+// })
+
+// 函数式编程
+const goodMilage = R.propSatisfies(R.lte(30), 'mpg')
+const fourDoors = R.propEq('doors', 4)
+const predicates = R.allPass([goodMilage, fourDoors])
+const result = R.filter(predicates, cars)
 
 console.log(result)
