61c4ff1bce0bcec8a493574c19c1897d26b12c50
郑俊鑫 <18810544291@163.com>
2017-05-01 17:04:57 +0800

    函数式编程

--- egghead/toPairs.js
+++ egghead/toPairs.js
@@ -1,5 +1,6 @@
 // 把对象参数转化为查询字符串
 
+const R = require('ramda')
 const qsObj = {
     page: 2,
     size: 10,
@@ -7,9 +8,15 @@ const qsObj = {
 }
 
 // 非函数式，需要自己实现 toPair
-const createQs = qsObj => '?' + Object.keys(qsObj)
-                                    .map(key => [key, qsObj[key]])
-                                    .map(pair => pair.join('='))
-                                    .join('&')
+// const createQs = qsObj => '?' + Object.keys(qsObj)
+//                                     .map(key => [key, qsObj[key]])
+//                                     .map(pair => pair.join('='))
+//                                     .join('&')
+const createQs = R.pipe(
+    R.toPairs,
+    R.map(R.join('=')),
+    R.join('&'),
+    R.concat('?')
+)
 const result = createQs(qsObj)
 console.log(result)
